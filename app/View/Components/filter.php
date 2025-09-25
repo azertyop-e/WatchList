cbd8ad272c75ce35646a570fe9fa3a652a0f2edd
EliottM <eliott.muller2411@gmail.com>
2025-09-25 14:32:12 +0200

    refactor: simplification des filtres

--- app/View/Components/filter.php
+++ app/View/Components/filter.php
@@ -6,141 +6,97 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use App\Models\Gender;
-use App\Models\Movie;
 
-class filter extends Component
+class Filter extends Component
 {
     public $genres;
     public $selectedGenre;
-    public $selectedType;
     public $currentRoute;
     public $currentParams;
     public $urls;
-    public $hasActiveFilters;
     public $context = 'unseen'|'seen';
 
     /**
      * Create a new component instance.
      */
-    public function __construct($selectedGenre = '', $selectedType = 'film', $currentRoute = 'home', $currentParams = [], $context = 'unseen')
+    public function __construct($selectedGenre = '', $currentRoute = 'home', $currentParams = [], $context = 'unseen')
     {
         $this->selectedGenre = $selectedGenre;
-        $this->selectedType = $selectedType;
         $this->currentRoute = $currentRoute;
         $this->currentParams = $currentParams;
         $this->context = $context;
         
         $this->genres = $this->getAvailableGenres();
-        
         $this->urls = $this->generateUrls();
-        $this->hasActiveFilters = $this->selectedGenre != '' || ($this->selectedType != 'all');
     }
 
     /**
-     * Récupère les genres des films et séries présents dans la liste selon le type sélectionné
+     * Récupère les genres des films et séries présents dans la liste
+     * 
+     * @return array Les genres disponibles pour le filtrage
      */
-    private function getAvailableGenres()
+    private function getAvailableGenres(): array
     {
-        $genres = collect();
+        $isSeen = $this->context === 'seen';
         
-        // Si le type est 'all', on récupère les genres des deux types
-        if ($this->selectedType === 'all') {
-            if ($this->context === 'seen') {
-                $movieGenres = Gender::whereHas('movies', function($query) {
-                    $query->where('is_seen', true);
-                })->get();
-                
-                $seriesGenres = Gender::whereHas('series', function($query) {
-                    $query->where('is_watched', true);
-                })->get();
-            } else {
-                $movieGenres = Gender::whereHas('movies', function($query) {
-                    $query->where('is_seen', false);
-                })->get();
-                
-                $seriesGenres = Gender::whereHas('series', function($query) {
-                    $query->where('is_watched', false);
-                })->get();
-            }
-            
-            $genres = $movieGenres->merge($seriesGenres);
-        } 
-        // Si le type est 'film', on récupère seulement les genres des films
-        elseif ($this->selectedType === 'film') {
-            if ($this->context === 'seen') {
-                $genres = Gender::whereHas('movies', function($query) {
-                    $query->where('is_seen', true);
-                })->get();
-            } else {
-                $genres = Gender::whereHas('movies', function($query) {
-                    $query->where('is_seen', false);
-                })->get();
-            }
-        } 
-        // Si le type est 'serie', on récupère seulement les genres des séries
-        elseif ($this->selectedType === 'serie') {
-            if ($this->context === 'seen') {
-                $genres = Gender::whereHas('series', function($query) {
-                    $query->where('is_watched', true);
-                })->get();
-            } else {
-                $genres = Gender::whereHas('series', function($query) {
-                    $query->where('is_watched', false);
-                })->get();
-            }
-        }
+        $genres = Gender::where(function ($query) use ($isSeen) {
+            $query->whereHas('movies', function ($movieQuery) use ($isSeen) {
+                $movieQuery->where('is_seen', $isSeen);
+            })->orWhereHas('series', function ($seriesQuery) use ($isSeen) {
+                $seriesQuery->where('is_watched', $isSeen);
+            });
+        })
+        ->orderBy('name')
+        ->get()
+        ->unique('id')
+        ->values()
+        ->all();
         
-        return $genres->unique('id')->sortBy('name');
+        return $genres;
     }
 
     /**
      * Génère toutes les URLs nécessaires
+     * 
+     * @return array Les URLs générées
      */
-    private function generateUrls()
+    private function generateUrls(): array
     {
         $urls = [
-            'all' => $this->buildUrl($this->selectedGenre, 'all'),
-            'film' => $this->buildUrl($this->selectedGenre, 'film'),
-            'serie' => $this->buildUrl($this->selectedGenre, 'serie'),
-            
-            'all_genres' => $this->buildUrl('', $this->selectedType),
-            
-            'remove_type' => $this->buildUrl($this->selectedGenre, 'all'),
-            'remove_genre' => $this->buildUrl('', $this->selectedType),
-            'remove_all' => $this->buildUrl('', 'all'),
+            'all_genres' => $this->buildUrl(''),
         ];
         
         foreach ($this->genres as $genre) {
-            $urls['genre_' . $genre->id] = $this->buildUrl($genre->id, $this->selectedType);
+            $urls['genre_' . $genre->id] = $this->buildUrl($genre->id);
         }
-        
         return $urls;
     }
 
     /**
      * Génère une URL avec les paramètres de filtrage
+     * 
+     * @param string|null $genre Le genre à filtrer
+     * @return string L'URL générée
      */
-    private function buildUrl($genre = null, $type = null)
+    private function buildUrl(?string $genre = null): string
     {
         $params = $this->currentParams;
         
-        if ($genre !== null) {
+        if (!empty($genre)) {
             $params['genre'] = $genre;
+        } else {
+            unset($params['genre']);
         }
         
-        if ($type !== null) {
-            $params['type'] = $type;
-        }
-        
-        $params = array_filter($params, function($value) {
-            return $value !== '' && $value !== null;
-        });
+        $params = array_filter($params, fn($value) => $value !== '' && $value !== null);
         
         return route($this->currentRoute, $params);
     }
 
     /**
      * Get the view / contents that represent the component
+     * 
+     * @return \Illuminate\Contracts\View\View|\Closure|string
      */
     public function render(): View|Closure|string
     {
